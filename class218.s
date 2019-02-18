b421b5ae076c4c536c198c2cdc16f23baa1a36d6
Noah <Franck.noahk@gmail.com>
2019-02-18 18:38:51 +0000

    now accepts a string and returns it & 255 spots in memory. So there will be extra fun stuff from memorty

--- class218.s
+++ class218.s
@@ -10,9 +10,26 @@ _start:
     LDR r1, =prompt
     swi 0
 
+read: 
+    mov r7, #3
+    mov r0, #0
+    mov r2, #255
+    ldr r1, =string
+    swi 0 
+
+write:
+    mov r7, #4
+    mov r0, #1
+    mov r2, #255
+    ldr r1, =string
+    swi 0
+
+
+
 end:
     mov r7, #1
     swi 0
 
 .data
 prompt: .ascii "Please enter a String:"
+string: .ascii "                                                                                       "
