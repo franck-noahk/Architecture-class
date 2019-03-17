aa7ea3b3cb27bcfb1a227fff4e8084ecd66afb37
Noah <Franck.noahk@gmail.com>
2019-03-17 18:31:10 +0000

    fixed compile error I think but not sure if it is saving anything yet. slowly working on print and read errors so that it will print and recieve data from the user correctly

--- calc.s
+++ calc.s
@@ -11,50 +11,30 @@
 _start:						@main goal is get user to give me a number 
 	mov r7, #4
 	mov r0, #1
-	mov r2, #34
+	mov r2, #36
 	ldr r1, =promptnumber
 	swi 0
 
 read:						@read in the value	
 	mov r7, #3
 	mov r0, #0
-	mov r2, #1
-	str r1, num1;			@having issues with getting it to work Maybe switching back to ldr & add swi 0?
-
+	mov r2, #10
+	ldr r1, =num1;			@having issues with getting it to work Maybe switching back to ldr & add swi 0?
+	swi 0
 prompt2:
 	mov r7, #4
 	mov r0, #1
-	mov r2, #34
+	mov r2, #54
 	ldr r1, =promptOpperand
 	swi 0
-
-readopp:
+readOpperand:
 	mov r7, #3
 	mov r0, #0
 	mov r2, #1
-	str r1, opp;
-
-prompNum2:
-	mov r7, #4
-	mov r0, #1
-	mov r2, #34
-	ldr r1, =promptnumber
-	swi 0
-
-getNum2:
-	mov r7, #3
-	mov r0, #0
-	mov r2, #1
-	str r1, num2
-
-decide:
-	ldr r1, =opp
-	ldr r0, =num1
-	ldr r2, =num2
-
-	mov r7, #4
-	mov r0, #1
-	mov r2, #3
+	ldr r1, =opp;			
+	swi 0	
+	@mov r0, #1
+	@mov r2, #3
 	
 	cmp r1, #49
 	BEQ addition
@@ -95,8 +75,8 @@ end:
 	swi 0 
 	
 .data
-promptnumber: .ascii "Please enter a non-negative number."
-promptOpperand: .Ascii "Please Enter 1. for +. 2. for -. 3. for *. 4. for /."
+promptnumber: .ascii "Please enter a non-negative number.\n"
+promptOpperand: .Ascii "Please Enter 1. for +. 2. for -. 3. for *. 4. for /.\n"
 num1: .space 255
 num2: .space 255
 opp: .space 255
