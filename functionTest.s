ee28f51d3288a273560aefe0f825ed058393ac19
Noah F <franck.noahk@gmail.com>
2019-04-08 17:58:21 +0000

    improved the work and comment for using arguments in gcc compiler

--- functionTest.s
+++ functionTest.s
@@ -1,9 +1,14 @@
 //Program to test function calls
 
 .data
-    greetings: .asciz "Hello, World!"
-    return : .word 0    //returning in c to make it happy
     .balign 4
+    greetings: .asciz "Hello, World!"
+    .balign 4
+    return: .word 0    //returning in c to make it happy
+    .balign 4
+    num1: .word 0       //for scanf
+    .balign 4
+    format_string: .asciiz "%d"
 .text
 
 .global main
@@ -14,8 +19,15 @@ main:
     str lr, [r1]                //store current link address into r1
 
     ldr r0, address_of_greeting //make r0 pointer to greeting
+    BL printf
 
-    bl puts
+    LDR r0, address_of_format_string
+    ldr r1, address_of_num1
+    bl scanf
+
+    ldr r0, address_of_num1
+    
+    bl printf
 
     ldr r1, address_of_return   //make r1 a pointer to return
     ldr lr, [r1]                //store value of r1 into r1
@@ -23,6 +35,7 @@ main:
 
 address_of_return: .word return
 address_of_greeting: .word greeting
-
+address_of_num1: .word num1
+address_of_num2: .word num2
 
 .global puts
