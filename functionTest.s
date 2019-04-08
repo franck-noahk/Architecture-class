ca94acced202528af4dd85b49c8730e878ee06c5
Noah F <franck.noahk@gmail.com>
2019-04-08 17:33:19 +0000

    Update comments to work with what were actually doing

--- functionTest.s
+++ functionTest.s
@@ -9,12 +9,20 @@
 .global main
 
 main:
-    ldr r0, greeting
-    str lr, [r1]
+
+    ldr r0, address_of_return   //loads address of load value
+    str lr, [r1]                //store current link address into r1
+
+    ldr r0, address_of_greeting //make r0 pointer to greeting
 
     bl puts
 
-    ldr lr, [r1]
+    ldr r1, address_of_return   //make r1 a pointer to return
+    ldr lr, [r1]                //store value of r1 into r1
     bx lr
+
+address_of_return: .word return
+address_of_greeting: .word greeting
+
 
 .global puts
