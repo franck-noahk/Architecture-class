d8f56527d68fc5a2e1b8759d54fb15ddb7a0efea
Noah F <franck.noahk@gmail.com>
2019-04-10 17:35:51 +0000

    Working on new multiply by 5 function

--- functionTest.s
+++ functionTest.s
@@ -8,8 +8,19 @@
     .balign 4
     num1: .word 0       //for scanf
     .balign 4
+    format_output: .asciiz "%d\n"
+    .balign 4
     format_string: .asciiz "%d"
 .text
+multiply5: 
+    add r1, r0, r0              // times 2
+    add r1, r1, r0              // times 3
+    add r1, r1, r0              // times 4
+    add r1, r1, r0              // times 5
+
+    mov r0, r1                  //moving return values to r0
+    
+    bx lr                       //way to leave the code
 
 .global main
 
@@ -25,8 +36,14 @@ main:
     ldr r1, address_of_num1
     bl scanf
 
+    //-----------------multiply by 5 ---------------------
     ldr r0, address_of_num1
-    
+    ldr r0, [r0]
+    bl multiply5                //go and see multiply5
+
+    mov r1, r0                  //getting ready for next printf
+
+    ldr r0, address_of_num1
     bl printf
 
     ldr r1, address_of_return   //make r1 a pointer to return
@@ -37,5 +54,7 @@ address_of_return: .word return
 address_of_greeting: .word greeting
 address_of_num1: .word num1
 address_of_num2: .word num2
+address_of_format_string: .word format_string
+address_of_format_output: .word format_output
 
 .global puts
