1300963e77f46f1d2090f330dee93cd1a7640078
Noah <Franck.noahk@gmail.com>
2019-04-12 16:46:44 +0000

    Formatting as well as working functions.

--- functionTest.s
+++ functionTest.s
@@ -14,14 +14,13 @@
     .balign 4
     format_string: .ascii "%d"
 .text
+
 multiply5: 
     add r1, r0, r0              // times 2
     add r1, r1, r0              // times 3
     add r1, r1, r0              // times 4
     add r1, r1, r0              // times 5
-
     mov r0, r1                  //moving return values to r0
-    
     bx lr          	//Unsure if bx is right here, ask Willie            //way to leave the code
 
 .global main
@@ -30,30 +29,23 @@ main:
 
     ldr r0, address_of_return   //loads address of load value
     str lr, [r1]                //store current link address into r1
-
     ldr r0, address_of_greeting //make r0 pointer to greeting
     BL printf
-
     LDR r0, address_of_format_string
     ldr r1, address_of_num1
     bl scanf
-
     //-----------------multiply by 5 ---------------------
     ldr r0, address_of_num1
     ldr r0, [r0]
     bl multiply5                //go and see multiply5
-
     mov r1, r0                  //getting ready for next printf
-
     ldr r0, address_of_format_output     // loading r0 with address for printf
     //-----------------multiply function call endend -----------------------
     bl printf
-
     ldr r1, address_of_return   //make r1 a pointer to return
     ldr lr, [r1]                //store value of r1 into r1
     bx lr
-	
-	mov r6, #0
+	mov r7, #1
 	swi 0
 
 
