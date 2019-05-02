61ce8bde9b09a13932e04efdb676c12bc9da4f2d
Noah <Franck.noahk@gmail.com>
2019-05-02 03:14:23 +0000

    Added the addressing that is needed

--- endgameCalculator.s
+++ endgameCalculator.s
@@ -1,12 +1,33 @@
 .globval main
 .data
-
-
+	.balign 4
+	promptNum1: .asciz “Please enter some number that you want to work with”
+	.balign 4
+	inputNum1String: .asciz “%d”
+	.balign 4
+	outputString: .asciz “Your answer is %d”
+	.balign 4
+	return: .word 0
+	.balign 4
+	signPrompt: .word “What do you want the numbers to do?\n1)add \n2)subtract\n 3)multiply\n4)divide”
 .align 2
 .text
-main:
- 
+main: 
+	ldr r1, addressOfReturn
+	str lr, [r1]
+	
+
 
 end:
-mov r7, #1
-swi 0
+	ldr r1, addressOfReturn
+	ldr lr, [r1]
+	by lr
+
+addressOfPromptNum1: .word promptNum1
+addressOfInputNum1String: .word inputNum1String
+addressOfOutputString: .word outputString
+addressOfReturn: .word return
+
+
+
+
