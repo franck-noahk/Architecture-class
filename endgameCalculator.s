6b18fb2b4e83fd754191fcd3f0bc555165e97820
Noah <Franck.noahk@gmail.com>
2019-05-08 03:27:52 +0000

    Added & filled int the promptUser tag.

--- endgameCalculator.s
+++ endgameCalculator.s
@@ -1,3 +1,6 @@
+//compiled with gcc compiler only
+
+
 .data
 	.balign 4
 	promptNum1: .asciz "Please enter some number that you want to work with"
@@ -9,7 +12,10 @@
 	return: .word 0
 	.balign 4
 	signPrompt: .asciz "What do you want the numbers to do?\n 1)add \n 2)subtract\n 3)multiply\n 4)divide"
-
+	.balign 4
+	answer: .word 0
+`	.balign 4 
+	signAnswer: .word 0
 .balign 2
 .text
 
@@ -17,8 +23,15 @@
 main: 
 	ldr r11, addressOfReturn
 	str lr, [r11]
-promptUser:
-
+promptUser:	
+	ldr r0, addressOfPromptNum1
+	bx printf
+	ldr r0, addressOfInputNum1String
+	ldr r1, addressOfAnswer		
+	bx scanf
+	ldr r0, addressOfSignPrompt
+	bx printf
+	ldr r0,  
 add:
 
 subtract:
@@ -34,11 +47,16 @@ end:
 	ldr r11, addressOfReturn
 	ldr lr, [r11]
 	bx lr
-
+//addresses
 addressOfPromptNum1: .word promptNum1
 addressOfInputNum1String: .word inputNum1String
 addressOfOutputString: .word outputString
 addressOfReturn: .word return
+addressOfAnswer: .word answer
+addressOfSignPrompt: .word signPrompt
+
+
+
 
 
 
